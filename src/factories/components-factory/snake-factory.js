c6b1c49df7e8ffcf54dad86d51c1af2ad94a0555
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-03-23 18:02:13 -0300

    refactor: adjust SnakeFactory to only handle with snake behavior

--- src/factories/components-factory/snake-factory.js
+++ src/factories/components-factory/snake-factory.js
@@ -1,90 +1,48 @@
-const {
-  ComponentsEnum: {
-    SNAKE_HEAD, SNAKE_BODY, TARGET, BRICK, EMPITY,
-  },
-  DirectionsEnum: {
-    RIGTH, LEFT, DOWN, UP,
-  },
-} = require('../../utils/enums');
+const { DirectionsEnum } = require('../../utils/enums');
 
 class SnakeFactory {
-  constructor({ session }) {
-    this.session = session;
-  }
-
-  makeSnake() {
-    this.session.snake = {
-      nextHeadPosition: {},
-      currentHeadPosition: { row: 2, column: 2 },
-      body: [{ row: 2, column: 1 }],
-      currentDirection: RIGTH,
+  constructor({ startingPosition, startingDirection  } = {}) {
+    const row = startingPosition.row || 2;
+    const column = startingPosition.column || 2;
+    const currentDirection = startingDirection || DirectionsEnum.RIGTH;
+    
+    this.snake = {
+      currentDirection,
+      body: [{ row, column }]
     };
-
-    this.updateSnake();
   }
 
-  move() {
-    const { board, snake, emitter } = this.session;
-    const { body, currentHeadPosition, nextHeadPosition } = snake;
+  move({ isScore, scoreHandler, isGameOver, gameOverHandler }) {
+    const { body } = this.snake;
+    body.unshift(this.getNextPosition());
 
-    const { row, column } = this.getNextPosition();
-    nextHeadPosition.row = row;
-    nextHeadPosition.column = column;
-
-    if (this.isGameOver()) throw new Error('GAME OVER');
-    if (this.isScore()) {
-      emitter.emit('score');
-    } else {
-      const highestIndex = body.length - 1;
-      board[body[highestIndex].row][body[highestIndex].column] = EMPITY;
-      body.pop();
-    }
-
-    body.unshift({ ...currentHeadPosition });
-    currentHeadPosition.row = row;
-    currentHeadPosition.column = column;
-
-    this.updateSnake();
+    if (isGameOver({ snake: this })) return gameOverHandler();
+    if (isScore({ snake: this })) return scoreHandler();
+    return body.pop();
   }
 
   getNextPosition() {
-    const { snake: { currentDirection, currentHeadPosition } } = this.session;
+    const { currentDirection } = this.snake;
 
+    const invalidDirection = !(currentDirection in DirectionsEnum);
+    if (invalidDirection) throw new Error('INVALID DIRECTION');
+
+    const currentHeadPosition = this.getHeadPosition();
     const options = {
-      [RIGTH]: () => ({ row: currentHeadPosition.row, column: currentHeadPosition.column + 1 }),
-      [LEFT]: () => ({ row: currentHeadPosition.row, column: currentHeadPosition.column - 1 }),
-      [DOWN]: () => ({ row: currentHeadPosition.row + 1, column: currentHeadPosition.column }),
-      [UP]: () => ({ row: currentHeadPosition.row - 1, column: currentHeadPosition.column }),
+      [DirectionsEnum.RIGTH]: { row: currentHeadPosition.row, column: currentHeadPosition.column + 1 },
+      [DirectionsEnum.LEFT]: { row: currentHeadPosition.row, column: currentHeadPosition.column - 1 },
+      [DirectionsEnum.DOWN]: { row: currentHeadPosition.row + 1, column: currentHeadPosition.column },
+      [DirectionsEnum.UP]: { row: currentHeadPosition.row - 1, column: currentHeadPosition.column },
     };
 
-    return options[currentDirection]();
+    return options[currentDirection];
   }
 
-  isGameOver() {
-    const { board, snake } = this.session;
-    const { nextHeadPosition: { row, column } } = snake;
+  getHeadPosition() {
+    const { body } = this.snake;
+    const headPosition = body[0];
 
-    const hitsBrick = board[row][column] === BRICK;
-    const hitsSnakeBody = board[row][column] === SNAKE_BODY;
-
-    return hitsBrick || hitsSnakeBody;
-  }
-
-  isScore() {
-    const { board, snake } = this.session;
-    const { nextHeadPosition: { row, column } } = snake;
-
-    return board[row][column] === TARGET;
-  }
-
-  updateSnake() {
-    const { board, snake } = this.session;
-    const { body, currentHeadPosition: { row, column } } = snake;
-
-    this.session.board[row][column] = SNAKE_HEAD;
-    for (let i = 0; i < body.length; i++) {
-      board[body[i].row][body[i].column] = SNAKE_BODY;
-    }
+    return headPosition;
   }
 }
 
