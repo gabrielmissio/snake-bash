0a70007c710ae0902723ebb2bc1d87b2064f1a6a
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-03-23 11:31:26 -0300

    chore: add prettier and adjust lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,8 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
