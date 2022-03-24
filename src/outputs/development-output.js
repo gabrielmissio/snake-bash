166a88403b4fb3312eb49ddd7df8be5739f2d800
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-03-23 23:46:01 -0300

    feat: add drawScore and drawGameOver to DevelopmentOutput

--- src/outputs/development-output.js
+++ src/outputs/development-output.js
@@ -2,6 +2,14 @@ class DevelopmentOutput {
   static drawBoard({ board }) {
     console.table(board);
   }
+
+  static drawScore({ score }) {
+    console.log(score);
+  }
+
+  static drawGameOver() {
+    console.log('GAME OVER');
+  }
 }
 
 module.exports = DevelopmentOutput;
